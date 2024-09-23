ad82a39d046930e8533d01870aa5c82dbd1d59fe
xingchentye <xingchenawa@qq.com>
2024-09-23 20:02:02 +0800

    commit: Collect and set up the data required by Aindrea

--- src/main/java/com/aindrea/com/aindrea/Aindrea.java
+++ src/main/java/com/aindrea/com/aindrea/Aindrea.java
@@ -1,5 +1,13 @@
 package com.aindrea.com.aindrea;
 
+import java.nio.file.FileSystems;
+
 public class Aindrea {
+    public static final String AindreaVersion = Version.getFullVersion();
+    public static final String AindreaWorkPath =  System.getProperty("user.dir");
+    public static final String SystemSeparator = FileSystems.getDefault().getSeparator();
+    public static final String SystemName = System.getProperty("os.name");
+    public static final String SystemArch = System.getProperty("os.arch");
+
 
 }
