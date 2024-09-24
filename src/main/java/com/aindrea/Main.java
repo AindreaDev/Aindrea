c1a047967ee0c60130253b5035aff464851589f6
xingchentye <xingchenawa@qq.com>
2024-09-24 22:00:39 +0800

    tuning: Change the storage type of the working directory to the Path type.

--- src/main/java/com/aindrea/Main.java
+++ src/main/java/com/aindrea/Main.java
@@ -4,8 +4,6 @@ import com.aindrea.aindrea.Aindrea;
 import com.aindrea.aindrea.utils.BasicWorkPath;
 import com.aindrea.aindrea.utils.LanguageConfig;
 
-import java.nio.file.Path;
-
 /**
  * author: xingchen
  * date: 2024/9/22
@@ -15,6 +13,6 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("file.encoding","UTF-8");
         LanguageConfig Language = new LanguageConfig();
-        new BasicWorkPath(Path.of(Aindrea.AindreaWorkPath));
+        new BasicWorkPath(Aindrea.AindreaWorkPath);
     }
 }
--- src/main/java/com/aindrea/aindrea/Aindrea.java
+++ src/main/java/com/aindrea/aindrea/Aindrea.java
@@ -1,10 +1,11 @@
 package com.aindrea.aindrea;
 
 import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 public class Aindrea {
     public static final String AindreaVersion = Version.getFullVersion();
-    public static final String AindreaWorkPath =  System.getProperty("user.dir");
+    public static final Path AindreaWorkPath =  Path.of(System.getProperty("user.dir"));
     public static final String SystemSeparator = FileSystems.getDefault().getSeparator();
     public static final String SystemName = System.getProperty("os.name");
     public static final String SystemArch = System.getProperty("os.arch");
